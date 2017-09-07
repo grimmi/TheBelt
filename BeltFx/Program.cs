99719444627503a7b618d3a88de5e32cb47450e8
Arne Lehmann <arne.lehmann@gmx.de>
2017-09-07 21:40:48 +0200

    .net 4.6.1 version for use with squirrel.windows; works ok, but not great...

---
+++ BeltFx/Program.cs
@@ -0,0 +1,31 @@
+﻿using Squirrel;
+using System;
+using System.Threading.Tasks;
+using TheBelt;
+
+namespace BeltFx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var mgr = new UpdateManager(@"c:\temp\releases"))
+            {
+                mgr.UpdateApp().Wait();
+            }
+
+            BeltSetup.Init();
+            Run().Wait();
+            BeltSetup.Teardown();
+            Console.ReadKey();
+        }
+
+        private static async Task Run()
+        {
+            var definition = BeltDefinition.FromFile("./parallelconfig.json");
+            var belt = definition.CreateBelt();
+
+            await belt.Run();
+        }
+    }
+}
